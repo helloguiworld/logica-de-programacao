827afd12ca7ad1b08fe7a9d3e5aea22bab84ebaf
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2023-03-09 16:23:11 -0300

    first commit

---
+++ rua.py
@@ -0,0 +1,11 @@
+
+
+chovendo = False
+orcamento = 99.99
+
+if (chovendo == True):
+    print("nao saio!")
+elif (orcamento >= 100):
+    print("saio pro rozidio!")
+else:
+    print("saio pro self service!")
